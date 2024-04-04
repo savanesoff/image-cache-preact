e3f70040292db42d600f3dbd4330cba1212939a0
Sam Avanesov <sam.avanesoff@gmail.com>
2024-04-04 12:55:03 -0400

    Comments added

--- src/memory/memory.ts
+++ src/memory/memory.ts
@@ -16,12 +16,22 @@ interface MemoryProps {
  * Its and abstraction that represents memory usage.
  * Emits events when size is overflowed, available or cleared.
  */
+/**
+ * Represents a Memory object that tracks the usage of memory.
+ */
 export class Memory extends Logger {
   private bytes = 0;
   readonly units: UnitsType;
   readonly size: number;
   private count = 0;
 
+  /**
+   * Creates a new Memory object.
+   * @param size - The size of the memory object.
+   * @param units - The units of the memory object size.
+   * @param logLevel - The log level for the memory object.
+   * @param name - The name of the memory object.
+   */
   constructor({
     size = 1,
     units = "GB",
@@ -41,8 +51,8 @@ export class Memory extends Logger {
   }
 
   /**
-   * Compiles a string with the status of the memory object you can log
-   * @returns Returns a string with the status of the memory object
+   * Compiles a string with the status of the memory object that can be logged.
+   * @returns A string with the status of the memory object.
    */
   getStatus(): string {
     return JSON.stringify(
@@ -56,6 +66,10 @@ export class Memory extends Logger {
     );
   }
 
+  /**
+   * Gets the free space in the memory object.
+   * @returns An object with the free space in bytes, units, and percentage.
+   */
   getFreeSpace() {
     const unitUsed = this.toUnits(this.bytes);
     const prsUsed = (unitUsed / this.size) * 100;
@@ -66,6 +80,10 @@ export class Memory extends Logger {
     };
   }
 
+  /**
+   * Gets the used space in the memory object.
+   * @returns An object with the used space in bytes, units, and percentage.
+   */
   getUsedSpace() {
     const unitUsed = this.toUnits(this.bytes);
     const prsUsed = (unitUsed / this.size) * 100;
@@ -76,6 +94,10 @@ export class Memory extends Logger {
     };
   }
 
+  /**
+   * Gets the average space in the memory object.
+   * @returns An object with the average space in bytes, units, and percentage.
+   */
   getAverage() {
     const unitUsed = this.toUnits(this.bytes);
     const prsUsed = (unitUsed / this.size) * 100;
@@ -87,6 +109,10 @@ export class Memory extends Logger {
     };
   }
 
+  /**
+   * Gets the state of the memory object.
+   * @returns An object with the count, size, units, and size in bytes of the memory object.
+   */
   getState() {
     return {
       count: this.count,
@@ -97,10 +123,10 @@ export class Memory extends Logger {
   }
 
   /**
-   * Adds bytes to the memory object and logs the status
-   * If adding bytes will overflow the memory object, not new bytes will be added.
-   * @param bytes - The number of bytes to add to the memory object
-   * @returns Returns the remaining bytes. If negative, the memory object is overflowed
+   * Adds bytes to the memory object and logs the status.
+   * If adding bytes will overflow the memory object, no new bytes will be added.
+   * @param bytes - The number of bytes to add to the memory object.
+   * @returns The remaining bytes. If negative, the memory object is overflowed.
    */
   addBytes(bytes: number): number {
     const remainingBytes = this.getBytesSpace(bytes);
@@ -127,9 +153,9 @@ export class Memory extends Logger {
   }
 
   /**
-   * Adds bytes in units to the memory object
-   * @param units
-   * @returns  Returns the remaining units. If negative, the memory object is overflowed
+   * Adds units to the memory object.
+   * @param units - The number of units to add to the memory object.
+   * @returns The remaining units. If negative, the memory object is overflowed.
    */
   addUnits(units: number): number {
     const remainingBytes = this.addBytes(units * UNITS[this.units]);
@@ -137,9 +163,9 @@ export class Memory extends Logger {
   }
 
   /**
-   * Calculates the remaining space in the memory object
-   * @param withBytes  - The number of bytes to add to the memory object
-   * @returns Returns the remaining bytes. If negative, the memory object is overflowed
+   * Calculates the remaining space in the memory object.
+   * @param withBytes - The number of bytes to add to the memory object.
+   * @returns The remaining bytes. If negative, the memory object is overflowed.
    */
   getBytesSpace(withBytes = 0): number {
     const remaining = this.size * UNITS[this.units] - (this.bytes + withBytes);
@@ -147,9 +173,10 @@ export class Memory extends Logger {
   }
 
   /**
-   * Removes bytes from the memory object and logs the status
-   * Emits available event if the memory object is not overflowed
-   * @param bytes - The number of bytes to remove from the memory object
+   * Removes bytes from the memory object and logs the status.
+   * Emits an "available" event if the memory object is not overflowed.
+   * @param bytes - The number of bytes to remove from the memory object.
+   * @returns The remaining bytes.
    */
   removeBytes(bytes: number): number {
     this.count--;
@@ -166,8 +193,8 @@ export class Memory extends Logger {
   }
 
   /**
-   * Clears the memory object and logs the status
-   * Emits clear event
+   * Clears the memory object and logs the status.
+   * Emits a "clear" event.
    */
   clear() {
     this.bytes = 0;
@@ -177,17 +204,29 @@ export class Memory extends Logger {
   }
 
   /**
-   * Logs the status of the memory object
+   * Logs the status of the memory object.
    */
   print() {
     this.log.info([this.getStatus()], this.styles.info);
   }
 
+  /**
+   * Overrides the `on` method to add event listeners to the memory object.
+   * @param event - The event to listen for.
+   * @param listener - The listener function to be called when the event is emitted.
+   * @returns The memory object itself.
+   */
   on(event: Events, listener: () => void): this {
     super.on(event, listener);
     return this;
   }
 
+  /**
+   * Overrides the `emit` method to emit events from the memory object.
+   * @param event - The event to emit.
+   * @param value - The value to pass to the event listeners.
+   * @returns A boolean indicating whether the event was emitted successfully.
+   */
   emit(event: Events, value?: unknown): boolean {
     return super.emit(event, this, value);
   }
