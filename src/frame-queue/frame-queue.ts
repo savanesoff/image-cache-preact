003b997fb449b58d5ff5aff46c13cb57ff526a9a
Sam Avanesov <sam.avanesoff@gmail.com>
2024-04-05 23:09:50 -0400

    set frame queue set

--- src/frame-queue/frame-queue.ts
+++ src/frame-queue/frame-queue.ts
@@ -7,7 +7,7 @@ type CB = () => void;
  */
 export class FrameQueue extends Logger {
   private scheduled = false;
-  readonly queue: CB[] = [];
+  readonly queue = new Set<CB>();
   constructor() {
     super({
       name: "BlitQueue",
@@ -18,14 +18,15 @@ export class FrameQueue extends Logger {
   private processQueue() {
     if (this.scheduled) return;
     this.scheduled = true;
-    const cb = this.queue.shift();
+    const cb = this.queue.values().next().value;
     if (!cb) {
       this.scheduled = false;
       return;
     }
-    this.log.verbose([`processing: ${this.queue.length}`]);
+    this.log.verbose([`processing: ${this.queue.size}`]);
     try {
       cb();
+      this.queue.delete(cb);
     } catch (e) {
       this.log.error([`Error processing callback: ${e}`]);
     }
@@ -38,7 +39,7 @@ export class FrameQueue extends Logger {
   }
 
   add(cb: CB) {
-    this.queue.push(cb);
+    this.queue.add(cb);
     this.processQueue();
     this.log.verbose(["added to queue"]);
   }
