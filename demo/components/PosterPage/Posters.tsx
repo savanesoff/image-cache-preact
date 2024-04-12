fffc7b0f900c3048973c08e0b39b59722be7d1d9
Sam Avanesov <sam.avanesoff@gmail.com>
2024-04-12 15:55:20 -0400

    Breakout poster componentes

---
+++ demo/components/PosterPage/Posters.tsx
@@ -0,0 +1,38 @@
+import { HTMLAttributes, useCallback, useState } from "react";
+import { ImageProvider } from "@/components/Image";
+import { useBucket } from "@/components/Bucket";
+import { Poster } from "../Poster/Poster";
+import { cn } from "@/utils";
+
+export type PosterProps = HTMLAttributes<HTMLDivElement> & {
+  urls: string[];
+  width: number;
+  height: number;
+};
+
+export const Posters = ({
+  urls,
+  width,
+  height,
+  className,
+  ...props
+}: PosterProps) => {
+  const [show, setShow] = useState(false);
+  const onRendered = useCallback(() => {
+    setShow(true);
+  }, []);
+  useBucket({ onRendered });
+
+  return (
+    <div
+      className={cn("flex flex-wrap gap-10", { "opacity-0": !show }, className)}
+      {...props}
+    >
+      {urls.map((url, index) => (
+        <ImageProvider key={index} url={url} width={width} height={height}>
+          <Poster />
+        </ImageProvider>
+      ))}
+    </div>
+  );
+};
