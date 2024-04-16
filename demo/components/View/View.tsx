5d22af58766ab7b9252e28a328d36d32c2608726
Sam Avanesov <sam.avanesoff@gmail.com>
2024-04-16 10:35:45 -0400

    Add toggle rail

--- demo/components/View/View.tsx
+++ demo/components/View/View.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@demo/utils";
 import { PostersRail } from "@demo/components";
 import { CacheStats } from "./CacheStats";
+import { useCallback, useState } from "react";
 
 /**
  * Example of an app view that uses the PostersRail component.
@@ -10,8 +11,22 @@ export const View = () => {
   return (
     <div className={cn("p-4", "bg-slate-600", "w-full")}>
       <CacheStats />
-      <PostersRail />
       {/* <PostersRail /> */}
+      <PostersRail />
+      <ToggleRail />
+    </div>
+  );
+};
+
+const ToggleRail = () => {
+  const [hidden, setHidden] = useState(true);
+  const toggleRail = useCallback(() => setHidden((hidden) => !hidden), []);
+  return (
+    <div className="mt-2 flex flex-col items-start gap-2">
+      <button className="rounded-md bg-blue-400 p-1 " onClick={toggleRail}>
+        Toggle Rail
+      </button>
+      {!hidden && <PostersRail />}
     </div>
   );
 };
