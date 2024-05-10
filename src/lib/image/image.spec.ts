fa1bc38a5b9fdff8a53beacebb47c96a479dddd8
Sam Avanesov <sam.avanesoff@gmail.com>
2024-05-09 23:28:24 -0400

    Image spec fixes

--- src/lib/image/image.spec.ts
+++ src/lib/image/image.spec.ts
@@ -1,28 +1,24 @@
-import { Img, RenderRequest } from "./image";
-import { Bucket } from "@/bucket";
+import { IMAGE_TYPE_BYTES, Img, Size } from "./image";
+import { RenderRequest } from "@lib/request";
+import { Bucket } from "@lib/bucket";
+import { Controller } from "@lib/controller";
 
 vi.useFakeTimers();
 const createBucket = (): Bucket => {
   return {
     emit: vi.fn(),
+    controller: {} as unknown as Controller,
   } as unknown as Bucket;
 };
 const blobData = "blob:test";
 const size = Math.round(Math.random() * 100);
 
-const createRequest = (size: number): [string, RenderRequest] => {
-  const key = `${size}x${size}`;
-  return [
-    key,
-    {
-      key: `${size}x${size}`,
-      size: { width: size, height: size },
-      bytes: 40000,
-      rendered: false,
-      requested: false,
-      buckets: new Set([createBucket(), createBucket()]),
-    },
-  ];
+const createRequest = (size: Size, bucket: Bucket): RenderRequest => {
+  return {
+    emit: vi.fn(),
+    bucket,
+    size,
+  } as unknown as RenderRequest;
 };
 
 describe("Img", () => {
@@ -46,18 +42,14 @@ describe("Img", () => {
     expect(image.element).toBeDefined();
   });
 
-  describe("on data loaded (see loader for details)", () => {
+  describe("on data loaded", () => {
     let sizeEventSpy: () => void;
-    let requestRenderSpy: () => void;
     beforeEach(() => {
       image.blob = new Blob(); // when loader is done the blob is set
       image.element.width = size; // because its hard to mock the image element
       image.element.height = size;
       sizeEventSpy = vi.fn();
-      requestRenderSpy = vi.fn();
-      image.on("request-render", requestRenderSpy);
       image.on("size", sizeEventSpy);
-      image.renderRequests.set(...createRequest(size));
       image.emit("loadend"); // called by loader
       image.element.onload?.(new Event("load")); // triggered by 'loadend' event
     });
@@ -81,199 +73,18 @@ describe("Img", () => {
       expect(sizeEventSpy).toHaveBeenCalledTimes(1);
     });
 
-    it("should emit size event with correct event prop names", () => {
+    it("should emit size event with correct props", () => {
       expect(sizeEventSpy).toHaveBeenCalledWith({
-        event: expect.anything(),
-        target: expect.anything(),
-        with: expect.anything(),
-        height: expect.anything(),
-      });
-    });
-
-    it("should emit size event with event name 'size'", () => {
-      expect(sizeEventSpy).toHaveBeenCalledWith({
-        event: "size",
-        target: expect.anything(),
-        with: expect.anything(),
-        height: expect.anything(),
-      });
-    });
-
-    it("should emit size event with correct width/height", () => {
-      expect(sizeEventSpy).toHaveBeenCalledWith({
-        event: expect.anything(),
-        target: expect.anything(),
-        with: size,
-        height: size,
-      });
-    });
-
-    it("should emit size event with correct target", () => {
-      expect(sizeEventSpy).toHaveBeenCalledWith({
-        event: expect.anything(),
+        type: "size",
         target: image,
-        with: expect.anything(),
-        height: expect.anything(),
+        size: { width: size, height: size },
       });
     });
 
-    it("should emit request-render event on image load cb", () => {
-      expect(requestRenderSpy).toHaveBeenCalled();
-    });
-
-    it("should emit request-render event with correct event prop names", () => {
-      expect(requestRenderSpy).toHaveBeenCalledWith({
-        event: expect.anything(),
-        target: expect.anything(),
-        request: expect.anything(),
-      });
-    });
-
-    it("should emit request-render event with event name 'request-render'", () => {
-      expect(requestRenderSpy).toHaveBeenCalledWith({
-        event: "request-render",
-        target: expect.anything(),
-        request: expect.anything(),
-      });
-    });
-
-    it("should emit request-render event with correct request", () => {
-      expect(requestRenderSpy).toHaveBeenCalledWith({
-        event: expect.anything(),
-        target: expect.anything(),
-        request: image.renderRequests.get(`${size}x${size}`),
-      });
-    });
-
-    it("should emit request-render event with correct target", () => {
-      expect(requestRenderSpy).toHaveBeenCalledWith({
-        event: expect.anything(),
-        target: image,
-        request: expect.anything(),
-      });
-    });
-  });
-
-  describe("request render", () => {
-    const requestLoadSpy = vi.fn();
-    beforeEach(() => {
-      image.gotSize = true;
-      image.renderRequests.set(...createRequest(size));
-      image.on("request-load", requestLoadSpy);
-      image.requestSize({
-        size: {
-          width: size,
-          height: size,
-        },
-        bucket: createBucket(),
-      });
-    });
-
-    it("should emit request-render event", () => {
-      expect(requestLoadSpy).toHaveBeenCalledTimes(1);
-    });
-
-    it("should not call requestLoad if request is already requested", () => {
-      image.requestSize({
-        size: {
-          width: size,
-          height: size,
-        },
-        bucket: createBucket(),
-      });
-      expect(requestLoadSpy).toHaveBeenCalledTimes(1);
-    });
-
-    it("should add new bucket to request", () => {
-      const bucket = createBucket();
-      image.requestSize({
-        size: {
-          width: size,
-          height: size,
-        },
-        bucket,
-      });
-      for (const request of image.renderRequests.values()) {
-        expect(request.buckets).toContain(bucket);
-      }
-    });
-
-    it("should not emit render-requested event if image not loaded", () => {
-      for (const bucket of image.renderRequests.values().next().value.buckets) {
-        expect(bucket.emit).not.toHaveBeenCalled();
-      }
-    });
-  });
-
-  describe("ClearSize()", () => {
-    let clearEventSpy: () => void;
-    beforeEach(() => {
-      clearEventSpy = vi.fn();
-      image.renderRequests.set(...createRequest(size));
-      image.on("size-cleared", clearEventSpy);
-      image.clearSize({ width: size, height: size });
-    });
-
-    it("should remove request from renderRequests", () => {
-      expect(image.renderRequests.size).toBe(0);
-    });
-
-    it("should emit clear-size event", () => {
-      expect(clearEventSpy).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe("onSizeRendered()", () => {
-    let sizeRenderedSpy: () => void;
-    beforeEach(() => {
-      sizeRenderedSpy = vi.fn();
-      image.renderRequests.set(...createRequest(size));
-      image.on("size-rendered", sizeRenderedSpy);
-      image.onSizeRendered(
-        image.renderRequests.get(`${size}x${size}`) as RenderRequest
+    it("should have bytesUncompressed set", () => {
+      expect(image.bytesUncompressed).toBe(
+        size * size * IMAGE_TYPE_BYTES[image.type],
       );
-    });
-
-    it("should set request.rendered to true", () => {
-      for (const request of image.renderRequests.values()) {
-        expect(request.rendered).toBe(true);
-      }
-    });
-
-    it("should emit size-rendered event", () => {
-      expect(sizeRenderedSpy).toHaveBeenCalledTimes(1);
-    });
-
-    it("should emit size-rendered event with correct event prop names", () => {
-      expect(sizeRenderedSpy).toHaveBeenCalledWith({
-        event: expect.anything(),
-        target: expect.anything(),
-        request: expect.anything(),
-      });
-    });
-
-    it("should emit size-rendered event with event name 'size-rendered'", () => {
-      expect(sizeRenderedSpy).toHaveBeenCalledWith({
-        event: "size-rendered",
-        target: expect.anything(),
-        request: expect.anything(),
-      });
-    });
-
-    it("should emit size-rendered event with correct request", () => {
-      expect(sizeRenderedSpy).toHaveBeenCalledWith({
-        event: expect.anything(),
-        target: expect.anything(),
-        request: image.renderRequests.get(`${size}x${size}`),
-      });
-    });
-
-    it("should emit size-rendered event with correct target", () => {
-      expect(sizeRenderedSpy).toHaveBeenCalledWith({
-        event: expect.anything(),
-        target: image,
-        request: expect.anything(),
-      });
     });
   });
 });
