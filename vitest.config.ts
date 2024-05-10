bce2aa6734664de1a07e3388bdcbebb41c72898e
Sam Avanesov <sam.avanesoff@gmail.com>
2024-05-09 23:23:40 -0400

    vitest config fix to include mock files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,10 @@
 import { defineConfig } from "vitest/config";
-import path from "path";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
-    environmentMatchGlobs: [
-      // all tests in tests/dom will run in jsdom
-      ["src/**", "jsdom"],
-    ],
-    alias: {
-      // @ts-expect-error - __dirname is not defined
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  resolve: {
-    alias: {
-      "@": "./src",
-    },
+    environment: "jsdom",
   },
 });
