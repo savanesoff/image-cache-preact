d1c9a8692cad40dd22b37ba920aa12e326d66f1a
Sam Avanesov <sam.avanesoff@gmail.com>
2024-05-13 16:24:25 -0400

    Image type utils added - not used

---
+++ src/utils/image-type.spec.ts
@@ -0,0 +1,31 @@
+import { getImageType, ImageType } from "./image-type";
+
+describe("getImageType", () => {
+  const testCases: [string, ImageType][] = [
+    ["89504e47", "image/png"],
+    ["47494638", "image/gif"],
+    ["ffd8ffe0", "image/jpeg"],
+    ["4d4d002a", "image/tiff"], // Big-endian format
+    ["424d", "image/bmp"],
+    // ["524946463839000057454250", "image/webp"], // Shortened to match RIFF format
+    // Add more cases as needed
+  ];
+
+  testCases.forEach(([header, type]) => {
+    it(`should return ${type} for header ${header}`, async () => {
+      const matched = header.match(/.{1,2}/g);
+      if (matched) {
+        const bytes = new Uint8Array(matched.map((byte) => parseInt(byte, 16)));
+        const blob = new Blob([bytes.buffer.slice(0, bytes.byteLength)]);
+        await expect(getImageType(blob)).resolves.toBe(type);
+      } else {
+        throw new Error("Header does not match expected format");
+      }
+    });
+  });
+
+  it("should reject for unknown image type", async () => {
+    const blob = new Blob([new Uint8Array([0, 0, 0, 0]).buffer]);
+    await expect(getImageType(blob)).rejects.toEqual("Unknown image type");
+  });
+});
---
+++ src/utils/image-type.ts
@@ -0,0 +1,62 @@
+export type ImageType =
+  | "image/png"
+  | "image/jpeg"
+  | "image/gif"
+  | "image/tiff"
+  | "image/bmp"
+  | "image/webp"
+  | "image/svg+xml"
+  | "image/vnd.microsoft.icon"
+  | "image/heic";
+
+export const getImageType = (blob: Blob): Promise<ImageType> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = function (e: ProgressEvent<FileReader>) {
+      if (e.target && e.target.readyState === FileReader.DONE) {
+        const arr = new Uint8Array(e.target.result as ArrayBuffer).subarray(
+          0,
+          12, // Read the first 12 bytes
+        );
+        let header = "";
+        for (let i = 0; i < arr.length; i++) {
+          header += arr[i].toString(16).padStart(2, "0"); // Ensure each byte is 2 characters long
+        }
+        switch (true) {
+          case /^89504e47/.test(header):
+            resolve("image/png");
+            break;
+          case /^47494638/.test(header):
+            resolve("image/gif");
+            break;
+          case /^ffd8ffe/.test(header):
+            resolve("image/jpeg");
+            break;
+          case /^49492a00/.test(header):
+          case /^4d4d002a/.test(header):
+            resolve("image/tiff");
+            break;
+          case /^424d/.test(header):
+            resolve("image/bmp");
+            break;
+          case /^52494646[0-9a-f]{16}/.test(header):
+            resolve("image/webp");
+            break;
+          case /^3c3f786d6c20/.test(header):
+          case /^3c73766720/.test(header):
+            resolve("image/svg+xml");
+            break;
+          case /^00000100/.test(header):
+            resolve("image/vnd.microsoft.icon");
+            break;
+          case /^6674797068656963/.test(header):
+            resolve("image/heic");
+            break;
+          default:
+            reject("Unknown image type");
+        }
+      }
+    };
+    reader.readAsArrayBuffer(blob.slice(0, 4));
+  });
+};
