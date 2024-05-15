7a8cff83f2fa5b1266169f2dd8425be8ba5a3269
Sam Avanesov <sam.avanesoff@gmail.com>
2024-05-15 09:36:28 -0400

    Split out rail

--- demo/components/PostersRail/PostersRail.tsx
+++ demo/components/PostersRail/PostersRail.tsx
@@ -1,22 +1,17 @@
-import { BucketProvider, useBucket } from "@/components";
-import { PosterPage, StatusBadge } from "@demo/components";
+import { BucketProvider } from "@/components";
 import { cn } from "@demo/utils";
 import { Topic } from "@demo/utils/assets.endpoint";
 import {
   FocusContext,
   useFocusable,
 } from "@noriginmedia/norigin-spatial-navigation";
-import { PageLoadStatus } from "../PosterPage/LoadStatus";
-import { RamUsage } from "../PosterPage/RamUsage";
-import { PageRenderStatus } from "../PosterPage/RenderStatus";
-import { VideoUsage } from "../PosterPage/VideoUsage";
-import { HTMLAttributes, useCallback, useState } from "react";
-import { BucketEvent } from "@/lib";
+import { HTMLAttributes } from "react";
+import { RailHeader } from "./RailHeader";
+import { Rail } from "./Rail";
 
 type PosterRailProps = HTMLAttributes<HTMLDivElement> & {
   topic: Topic;
   fromPage?: number;
-  assetCount?: number;
 };
 /**
  * An example of poster rail that fetches data and renders cached posters
@@ -24,7 +19,6 @@ type PosterRailProps = HTMLAttributes<HTMLDivElement> & {
 export const PostersRail = ({
   topic,
   fromPage = 0,
-  assetCount = 10,
   className,
   ...props
 }: PosterRailProps) => {
@@ -35,50 +29,13 @@ export const PostersRail = ({
   });
 
   return (
-    <BucketProvider name={topic.title}>
-      <FocusContext.Provider value={focusKey}>
-        <div className={cn("flex flex-col", className)} {...props}>
-          <div className="flex w-full flex-row items-center space-x-2 bg-slate-800 p-2 text-sm text-slate-400">
-            <div>Page </div>
-            <AssetCount />
-            <PageLoadStatus />
-            <PageRenderStatus />
-            <RamUsage />
-            <VideoUsage />
-          </div>
-          <div
-            ref={ref}
-            data-testid="rail"
-            className={cn(
-              "no-scrollbar flex h-[210px] flex-row overflow-y-hidden overflow-x-scroll bg-slate-900",
-              hasFocusedChild && "bg-fuchsia-950",
-            )}
-            title={topic.description}
-          >
-            {/* <div>{topic.title}</div> */}
-            {/* Lock the first page */}
-            <PosterPage name="Poser page main" topic={topic} pageNumber={0} />
-            {/* <PosterPage name="Poser page main" urls={urls} /> */}
-            {/* <PosterPage name="Poser page main" urls={urlsMain} /> */}
-          </div>
+    <FocusContext.Provider value={focusKey}>
+      <BucketProvider name={topic.title}>
+        <div className={cn("flex flex-col", className)} {...props} ref={ref}>
+          <RailHeader topic={topic} focused={hasFocusedChild} />
+          <Rail topic={topic} fromPage={fromPage} focused={hasFocusedChild} />
         </div>
-      </FocusContext.Provider>
-    </BucketProvider>
-  );
-};
-
-const AssetCount = () => {
-  const [imageCount, setImageCount] = useState(0);
-  const [requestCount, setRequestCount] = useState(0);
-  const onUpdate = useCallback((event: BucketEvent<"update">) => {
-    setImageCount(event.images);
-    setRequestCount(event.requests);
-  }, []);
-  useBucket({ onUpdate });
-  return (
-    <>
-      <StatusBadge text={`I: ${imageCount}`} />
-      <StatusBadge text={`R: ${requestCount}`} />
-    </>
+      </BucketProvider>
+    </FocusContext.Provider>
   );
 };
---
+++ demo/components/PostersRail/Rail.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@demo/utils";
+import { Topic } from "@demo/utils/assets.endpoint";
+import { HTMLAttributes } from "react";
+import { PosterPage } from "../PosterPage";
+
+export type RailProps = HTMLAttributes<HTMLDivElement> & {
+  focused: boolean;
+  topic: Topic;
+  fromPage: number;
+};
+export const Rail = ({
+  focused,
+  topic,
+  fromPage,
+  className,
+  ...props
+}: RailProps) => {
+  return (
+    <div
+      data-testid="rail"
+      className={cn(
+        "no-scrollbar flex h-[210px] flex-row space-x-2 overflow-y-hidden overflow-x-scroll bg-slate-900",
+        focused && "bg-fuchsia-950",
+        className,
+      )}
+      title={topic.description}
+      {...props}
+    >
+      {Array.from({ length: topic.pages }).map((_, index) => (
+        <PosterPage
+          key={index}
+          name="Poser page main"
+          topic={topic}
+          pageNumber={index + fromPage}
+        />
+      ))}
+    </div>
+  );
+};
