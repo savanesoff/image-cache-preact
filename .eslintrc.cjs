c41840d00d5b504dc6856f3b0359ee0c4680cbf9
Sam Avanesov <sam.avanesoff@gmail.com>
2024-05-21 15:34:17 -0400

    Disabled path validation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,54 +10,57 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh", "@typescript-eslint", "import"],
+  plugins: ["react-refresh", "@typescript-eslint"],
   rules: {
     "react-refresh/only-export-components": "warn",
     "@typescript-eslint/no-unused-vars": "error",
     "no-console": "warn",
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        ts: "never",
-        tsx: "never",
-        js: "always",
-        jsx: "always",
-      },
-    ],
-    "import/no-unresolved": [
-      "error",
-      { commonjs: true, caseSensitive: true },
-    ],
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: ["*.ts", "!*.d.ts", "@lib", "./*"],
-      },
-    ],
-    "import/order": [
-      "error",
-      {
-        groups: [["builtin", "external", "internal"]],
-        "newlines-between": "always",
-        alphabetize: { order: "asc", caseInsensitive: true },
-      },
-    ],
+    // "import/extensions": [
+    //   "error",
+    //   "ignorePackages",
+    //   {
+    //     ts: "never",
+    //     tsx: "never",
+    //     js: "always",
+    //     jsx: "always",
+    //   },
+    // ],
+    // "import/no-unresolved": [
+    //   "error",
+    //   {
+    //     commonjs: true,
+    //     caseSensitive: true,
+    //   },
+    // ],
+    // "no-restricted-imports": [
+    //   "error",
+    //   {
+    //     patterns: ["*.ts", "!*.d.ts"],
+    //   },
+    // ],
+    // "import/order": [
+    //   "error",
+    //   {
+    //     groups: [["builtin", "external", "internal"]],
+    //     "newlines-between": "always",
+    //     alphabetize: { order: "asc", caseInsensitive: true },
+    //   },
+    // ],
   },
   settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
-    },
-    "import/resolver": {
-      typescript: {
-        alwaysTryTypes: true,
-        project: "./tsconfig.json",
-      },
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
+    // "import/parsers": {
+    //   "@typescript-eslint/parser": [".ts", ".tsx"],
+    // },
+    // "import/resolver": {
+    //   typescript: {
+    //     alwaysTryTypes: true,
+    //     project: "./tsconfig.json",
+    //   },
+    //   node: {
+    //     extensions: [".js", ".jsx", ".ts", ".tsx"],
+    //   },
+    // },
   },
 };
