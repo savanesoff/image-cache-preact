ba67b2864096e89bc360d71bba926065f131aff7
Sam Avanesov <sam.avanesoff@gmail.com>
2024-05-21 08:54:58 -0400

    eslint extentions config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,66 @@
+const path = require('path');
+
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:react/recommended",
-    "prettier"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'import',
+  ],
   rules: {
-    "react-refresh/only-export-components": "warn",
-    "@typescript-eslint/no-unused-vars": "error",
-    "no-console": "warn",
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': 'warn',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'always',
+        jsx: 'always',
+      },
+    ],
+    'import/no-unresolved': [
+      'error',
+      { commonjs: true, caseSensitive: true }
+    ],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['*.ts', '!*.d.ts'],
+      },
+    ],
+    'react/react-in-jsx-scope': 'off', // Disable this rule
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: path.resolve(__dirname, './tsconfig.package.json'),
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
