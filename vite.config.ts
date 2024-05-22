25538e85c9f34ca966b6f253f7604885ac8b3cd1
Sam Avanesov <sam.avanesoff@gmail.com>
2024-05-22 15:39:27 -0400

    Vite esm cjs build configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import typescript from "@rollup/plugin-typescript";
 import { fileURLToPath } from "url";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -13,28 +12,23 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "ImageCacheReact",
-      formats: ["es", "cjs"],
-      fileName: (format) => `index.${format}.js`,
     },
-    sourcemap: true,
+    sourcemap: true, // Enable source maps
     rollupOptions: {
       external: ["react", "react-dom", "tslib"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          tslib: "tslib",
+      output: [
+        {
+          format: "es",
+          entryFileNames: "[name].js",
+          dir: "dist/esm",
+          sourcemap: true,
         },
-      },
-      plugins: [
-        typescript({
-          tsconfig: path.resolve(__dirname, "tsconfig.package.json"),
-          declaration: true,
-          declarationDir: path.resolve(__dirname, "dist/types"),
-          rootDir: path.resolve(__dirname, "src"),
-          sourceMap: true,
-          inlineSources: true,
-        }),
+        {
+          format: "cjs",
+          entryFileNames: "[name].js",
+          dir: "dist/cjs",
+          sourcemap: true,
+        },
       ],
     },
   },
