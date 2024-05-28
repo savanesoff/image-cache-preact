141f17b616f7c1b76854fbcd4e963f01617bbd1a
Sam Avanesov <sam.avanesoff@gmail.com>
2024-05-28 16:36:18 -0400

    render props fix

--- src/lib/request/renderer.ts
+++ src/lib/request/renderer.ts
@@ -1,33 +1,32 @@
 import { RenderRequest } from '@lib/request/index.js';
 
 export const renderer = ({
-  request,
+  target,
   renderTime,
 }: {
-  request: RenderRequest;
+  target: RenderRequest;
   renderTime: number;
 }) => {
   // create div of w/h set opacity to 0.1 append to body, ren remove on next frame
   const div = document.createElement('div');
   const style = {
-    width: `${request.size.width}px`,
-    height: `${request.size.height}px`,
+    width: `${target.size.width}px`,
+    height: `${target.size.height}px`,
     opacity: '0.001',
     position: 'absolute',
     top: '0',
     left:
-      Math.round(Math.random() * (window.innerWidth - request.size.width)) +
+      Math.round(Math.random() * (window.innerWidth - target.size.width)) +
       'px',
-    backgroundImage: `url(${request.image.url})`,
+    backgroundImage: `url(${target.image.url})`,
     // backgroundSize: "cover",
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'top left',
     pointerEvents: 'none',
-    backgroundSize: `${request.size.width}px ${request.size.height}px`,
+    backgroundSize: `${target.size.width}px ${target.size.height}px`,
   };
   Object.assign(div.style, style);
   document.body.appendChild(div);
-
   setTimeout(() => {
     document.body.removeChild(div);
   }, renderTime);
