22a82bf4b4fb59b6545d5534903fa43cc90ece36
Sam Avanesov <sam.avanesoff@gmail.com>
2024-06-03 22:02:23 -0400

    Using both react and preact plugins for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -14,26 +14,27 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'ImageCachePreact',
     },
-    sourcemap: true, // Enable source maps
+    sourcemap: true,
     rollupOptions: {
-      external: ['preact', 'tslib'],
+      external: ['react', 'tslib'],
       output: [
         {
           format: 'es',
           entryFileNames: '[name].js',
           dir: 'dist/esm',
-          sourcemap: true,
+          // sourcemap: true,
         },
         {
           format: 'cjs',
           entryFileNames: '[name].js',
           dir: 'dist/cjs',
-          sourcemap: true,
+          // sourcemap: true,
         },
       ],
     },
   },
-  plugins: [preact(), tsconfigPaths()],
+  // must use react for the package to build properly, but we'll use preact too since its a preact package (???)
+  plugins: [preact(), react(), tsconfigPaths()],
   resolve: {
     alias: {
       react: 'preact/compat',
